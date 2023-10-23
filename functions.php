83f8abc32d6ef67a9192ab0960e1d1323af205db
believe037 <engr.believenduka@gmail.com>
2023-10-23 15:27:51 +0100

    bootstrap cdn

--- functions.php
+++ functions.php
@@ -14,12 +14,16 @@
     // Register Styles.
     wp_register_style( 'styles-css', get_stylesheet_uri(),  [], filemtime( get_template_directory() . '/style.css' ), 'all' );
 
-    wp_register_style( 'bootstrap-css', get_template_directory() . '/assets/src/library/css/bootstrap.min.css',  [], false, 'all' );
+    wp_register_style( 'bootstrap-css', 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css',  [], false, 'all' );
 
     // Register Scripts.
     wp_register_script( 'main-js', get_template_directory_uri() . '/assets/main.js', [], filemtime( get_template_directory() . '/assets/main.js' ), true );
 
-    wp_register_script( 'bootstrap-js', get_template_directory_uri() . '/assets/src/library/js/bootstrap.min.js', [ 'jquery' ], false, true );
+    wp_register_script( 'bootstrap-js','https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js', [ 'jquery' ], false, true );
+
+    wp_register_script( 'bootstrap-bundle-js','https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js', [ 'jquery' ], false, true );
+
+
 
     // Enqueue Styles.
     wp_enqueue_style( 'style-css' );
@@ -28,6 +32,7 @@
     // Enqueue Scripts
     wp_enqueue_script( 'main-js' );
     wp_enqueue_script( 'bootstrap-js' );
+    wp_enqueue_script( 'bootstrap-bundle-js' );
  }
 
  add_action( 'wp_enqueue_scripts', 'blogBelieve_enqueue_scripts' );
